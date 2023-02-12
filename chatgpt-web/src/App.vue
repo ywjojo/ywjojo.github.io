6a3cd69ff4f5e18eb62d8140485d4759bc8199c6
ywjojo <125211096+ywjojo@users.noreply.github.com>
2023-02-13 03:20:24 +0800

    Add files via upload

---
+++ chatgpt-web/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+
+      <MainPage/>
+
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+  },
+  data() {
+    return {
+      loading: true
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+  },
+  
+  methods:{
+    startLoading(){
+      this.loading = true
+    },
+    finishLoading(){
+      this.loading = false
+    }
+  }
+}
+</script>
+
